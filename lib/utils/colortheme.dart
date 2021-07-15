e2b1aa7256cdfd0138e6b38f6daffdddd5b79d24
AkaBlaster <ns85596@gmail.com>
2021-07-15 19:18:13 +0530

    Splash Screen Functionality

---
+++ lib/utils/colortheme.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+Color white = Color(0xffffffff);
+Color yellow = Color(0xffFFD523);
+Color black = Color(0xff2C2E43);
+Color lightyellow = Color(0xffFFC947);
+Color blackish = Color(0xff0A1931);
